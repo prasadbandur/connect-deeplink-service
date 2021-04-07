75f135d4d19f8e98c26a16776d9f61f621e74aad
prasad <renuka.bandur@omio.com>
2021-04-07 17:39:44 +0200

    FND-2201: Initial Spring boot related structure.

---
+++ src/main/java/com/goeuro/connectdeeplinkservice/ConnectDeeplinkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goeuro.connectdeeplinkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectDeeplinkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectDeeplinkServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goeuro/connectdeeplinkservice/ConnectDeeplinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goeuro.connectdeeplinkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectDeeplinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
